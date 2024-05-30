7b8ac3fcc91b0a05b5de3d73d5294859a46506b2
Michael Mroz <michaelmroz01111@gmail.com>
2024-05-30 00:12:21 -0300

    add react-router and set up basic starting components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return(
+        <>
+        <nav>Hi</nav>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/components/StartingScreen.jsx
@@ -0,0 +1,11 @@
+const StartingScreen = () => {
+    return(
+        <>
+            <p>Please Choose Between a Default or Custom Starting Inventory</p>
+            <button>Default Inventory</button>
+            <button>Custom Inventory</button>
+        </>
+    )
+}
+
+export default StartingScreen
