6b8be993718e34d3cfb8e232427a3d9338f5e5d0
Michael Mroz <michaelmroz01111@gmail.com>
2024-06-28 10:38:12 -0300

    increase allowed length of product names

--- mongodb/ProductSchema.js
+++ mongodb/ProductSchema.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
     id: Schema.Types.ObjectId,
-    name: { type: String, minlength: 2, maxlength: 14, required: true },
+    name: { type: String, minlength: 2, maxlength: 20, required: true },
     stock: { type: Number, min: 0, max: 999, default: 0 },
     serialCode: { type: String, required: true }
 })
--- server/models/Product.js
+++ server/models/Product.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    name: { type: String, minlength: 2, maxlength: 16, required: true },
+    name: { type: String, minlength: 2, maxlength: 25, required: true },
     stock: { type: Number, min: 0, max: 999, default: 0 },
     serialCode: { type: String, required: true }
 })
